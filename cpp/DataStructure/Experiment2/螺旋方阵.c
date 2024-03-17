dcdf327af67f41b87a624bad072529442b3a78c5
BaoZhuhan <baozhuhan2004@163.com>
2024-03-17 21:38:59 +0800

    experiment 螺旋方阵2

--- cpp/DataStructure/Experiment2/螺旋方阵.c
+++ cpp/DataStructure/Experiment2/螺旋方阵.c
@@ -3,26 +3,53 @@
 #include <stdbool.h>
 #include <string.h>
 
+int a[10][10] = {0};
+int n ;
 
-void run(){
-    int n;
-    scanf("%d" , &n);
-
-    int a[100][100];
-    memset(a, 0 , sizeof(n));
-
-    int px = 0 , py = 0 ;  
-    for(int i = 1 ; i <= n*n ; i++){
-        if(i <= n){
-            py = i - 1;
-            a[px][py] = i;
-        }
-        else if(i <= 2*n)
+int fill(int layer,int len,int val){
+    for(int i = layer ; i < layer + len ; i++){
+        val = val + 1;
+        if(val > n*n ) break;
+        a[layer][i] = val;
     }
+
+    for(int i = layer + 1 ; i < layer + len - 1 ; i++){
+        val = val + 1;
+        if(val > n*n ) break;
+        a[i][layer + len - 1] = val;
+    }
+
+    for(int i = layer + len - 1 ; i >= layer ; i--){
+        val = val + 1;
+        if(val > n*n ) break;
+        a[layer + len - 1][i] = val;
+    }
+
+    for(int i = layer + len - 2 ; i > layer ; i--){
+        val = val + 1;
+        if(val > n*n ) break;
+        a[i][layer] = val;
+    }
+
+    return val;
 }
 
 int main(){
-    int T = 1;
-    while(T--) run();
+    
+    scanf("%d",&n);
+
+    int val = 0;
+    
+    for(int i = n ; i > 0 ; i -= 2){
+        val = fill((n-i)/2 , i , val);
+    }
+    
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n ; j++){
+            printf("%3d",a[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
