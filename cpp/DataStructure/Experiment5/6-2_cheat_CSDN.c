9817b0c379d8c54571efd2ddffdf32b3c53394c7
bao zhuhan <baozhuhan2004@163.com>
2024-04-13 11:06:42 +0800

    fine , i solve it

---
+++ cpp/DataStructure/Experiment5/6-2_cheat_CSDN.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stud_node {
+    int num;
+    char name[20];
+    int score;
+    struct stud_node* next;
+};
+
+struct stud_node* createlist();
+struct stud_node* deletelist( struct stud_node* head, int min_score );
+
+int main() {
+    int min_score;
+    struct stud_node* p, * head = NULL;
+
+    head = createlist();
+    scanf( "%d", &min_score );
+    head = deletelist( head, min_score );
+    for (p = head; p != NULL; p = p->next)
+        printf( "%d %s %d\n", p->num, p->name, p->score );
+
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+
+struct stud_node* createlist() {
+    struct stud_node* head = NULL, * tail = NULL, * s;
+    while (1) {
+        s = (struct stud_node*)malloc( sizeof( struct stud_node ) );
+        scanf( "%d", &s->num );
+        if (s->num == 0)
+            break;
+        scanf( "%s", s->name );
+        scanf( "%d", &s->score );
+        s->next = NULL;
+        if (head == NULL) {
+            head = s;
+        }
+        else {
+            tail->next = s;
+        }
+        tail = s;
+    }
+    return head;
+}
+
+struct stud_node* deletelist( struct stud_node* head, int min_score ) {
+    struct stud_node* p, * s;
+    while (head != NULL && head->score < min_score) {
+        p = head;
+        head = head->next;
+        free( p );
+    }
+    if (head == NULL)
+        return NULL;
+    s = head;
+    p = head->next;
+    while (p != NULL) {
+        if (p->score < min_score) {
+            s->next = p->next;
+            free( p );
+        }
+        else {
+            s = p;
+        }
+        p = s->next;
+    }
+    return head;
+}
--- cpp/DataStructure/Experiment5/6-6_linked_list_with_head_op_set.c
+++ cpp/DataStructure/Experiment5/6-6_linked_list_with_head_op_set.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 #define ERROR NULL
-typedef enum {false, true} bool;
+typedef enum { false, true } bool;
 typedef int ElementType;
-typedef struct LNode *PtrToLNode;
+typedef struct LNode* PtrToLNode;
 struct LNode {
     ElementType Data;
     PtrToLNode Next;
@@ -13,13 +13,12 @@ struct LNode {
 typedef PtrToLNode Position;
 typedef PtrToLNode List;
 
-List MakeEmpty(); 
+List MakeEmpty();
 Position Find( List L, ElementType X );
 bool Insert( List L, ElementType X, Position P );
 bool Delete( List L, Position P );
 
-int main()
-{
+int main() {
     List L;
     ElementType X;
     Position P;
@@ -27,103 +26,78 @@ int main()
     bool flag;
 
     L = MakeEmpty();
-    scanf("%d", &N);
-    while ( N-- ) {
-        scanf("%d", &X);
-        flag = Insert(L, X, L->Next);
-        if ( flag==false ) printf("Wrong Answer\n");
+    scanf( "%d", &N );
+    while (N--) {
+        scanf( "%d", &X );
+        flag = Insert( L, X, L->Next );
+        if (flag == false) printf( "Wrong Answer\n" );
     }
-    scanf("%d", &N);
-    while ( N-- ) {
-        scanf("%d", &X);
-        P = Find(L, X);
-        if ( P == ERROR )
-            printf("Finding Error: %d is not in.\n", X);
+    scanf( "%d", &N );
+    while (N--) {
+        scanf( "%d", &X );
+        P = Find( L, X );
+        if (P == ERROR)
+            printf( "Finding Error: %d is not in.\n", X );
         else {
-            flag = Delete(L, P);
-            printf("%d is found and deleted.\n", X);
-            if ( flag==false )
-                printf("Wrong Answer.\n");
+            flag = Delete( L, P );
+            printf( "%d is found and deleted.\n", X );
+            if (flag == false)
+                printf( "Wrong Answer.\n" );
         }
     }
-    flag = Insert(L, X, NULL);
-    if ( flag==false ) printf("Wrong Answer\n");
+    flag = Insert( L, X, NULL );
+    if (flag == false) printf( "Wrong Answer\n" );
     else
-        printf("%d is inserted as the last element.\n", X);
-    P = (Position)malloc(sizeof(struct LNode));
-    flag = Insert(L, X, P);
-    if ( flag==true ) printf("Wrong Answer\n");
-    flag = Delete(L, P);
-    if ( flag==true ) printf("Wrong Answer\n");
-    for ( P=L->Next; P; P = P->Next ) printf("%d ", P->Data);
+        printf( "%d is inserted as the last element.\n", X );
+    P = (Position)malloc( sizeof( struct LNode ) );
+    flag = Insert( L, X, P );
+    if (flag == true) printf( "Wrong Answer\n" );
+    flag = Delete( L, P );
+    if (flag == true) printf( "Wrong Answer\n" );
+    for (P = L->Next; P; P = P->Next) printf( "%d ", P->Data );
     return 0;
 }
 /* 你的代码将被嵌在这里 */
 
-List MakeEmpty(){
-    List H;
-    H = (List)malloc(sizeof(struct LNode));
-    H->Next = NULL;
-    return H;
+List MakeEmpty() {
+    List L = (List)malloc( sizeof( List ) );
+    L->Next = NULL;
+    return L;
 }
-
-Position Find( List L, ElementType X ){
+Position Find( List L, ElementType X ) {
     List p = L->Next;
-    while(p){
-        if(p->Data == X) return p;
+    while (p) {
+        if (p->Data == X) return p;
         p = p->Next;
     }
     return ERROR;
 }
-
-bool Insert( List L, ElementType X, Position P ){
+bool Insert( List L, ElementType X, Position P ) {
     List p = L;
-    List now;
-    now = (List)malloc(sizeof(struct LNode));
-    now->Data = X;
-
-    if(P == NULL){
-        while(p->Next != NULL){
-            p = p->Next;
-        }
-        p->Next = now;
-        now->Next = NULL;
-        return true;
-    }
-
-    while(p){
-        if(p->Next == P){
-            now->Next = P->Next;
-            p->Next = now;
+    List q = (List)malloc( sizeof( List ) );
+    q->Data = X;
+    while (p) {
+        if (p->Next == P) {		//即使P指向第一个节点，p仍有Next，所以无需特殊处理
+            q->Next = p->Next;
+            p->Next = q;
             return true;
         }
         p = p->Next;
     }
-
-    free(now);
-    printf("Wrong Position for Insertion\n");
+    printf( "Wrong Position for Insertion\n" );
+    return false;
+}
+bool Delete( List L, Position P ) {
+    List p = L;
+    while (p) {
+        if (p->Next == P) {
+            p->Next = P->Next;
+            free( P );
+            return true;
+        }
+        p = p->Next;
+    }
+    printf( "Wrong Position for Deletion\n" );
     return false;
 }
 
-bool Delete( List L, Position P ){
-    List p = L;
-
-    if(p->Next == NULL){
-        printf("Wrong Position for Deletion\n");
-        return false;
-    }
-
-    while(p->Next != P && p->Next != NULL){
-        p = p->Next;
-    }
-
-    if(p->Next == NULL) {
-        printf("Wrong Position for Deletion\n");
-        return false;
-    }
-    else{
-        List temp = p->Next;
-        p->Next = temp->Next;
-        return true;
-    }
-}
